84fd8e039ad2dd930855e3da6d4cb9361cd0a30b
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-12-14 21:56:37 +0500

    Add validation for user, post creating routes

--- routes/account.js
+++ routes/account.js
@@ -3,19 +3,40 @@ const router = express.Router();
 const api = require('../api');
 const issueJWT = require('../auth/issueJWT');
 const bcrypt = require('bcryptjs');
+const { body, validationResult } = require('express-validator');
+
+
+const createCredentialsValidator = () => ([
+  body('email', 'Email should have structure: mymail@example.com')
+  .isEmail(),
+  body('password').isStrongPassword()
+])
 
 // create a new user
-router.post('/signup', async (req, res, next) => {
-  let user;
-  try {
-    user = await api.createUser(req.body.email, req.body.password);
-  } catch(err) {
-    return next(err);
-  }
+router.post('/signup',
+  createCredentialsValidator(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const aggregateError = new AggregateError(
+        errors.array().map((err) => new Error(err.msg)),
+        'Invalid input'
+      );
+      
+      return next(aggregateError);
+    }
 
-  const jwt = issueJWT({id: user.id});
-  res.json(jwt);
-});
+    let user;
+    try {
+      user = await api.createUser(req.body.email, req.body.password);
+    } catch(err) {
+      return next(err);
+    }
+
+    const jwt = issueJWT({id: user.id});
+    res.json(jwt);
+  }
+);
 
 // verify credentials and get a jwt 
 router.post('/login', async (req, res, next) => {
--- routes/posts.js
+++ routes/posts.js
@@ -8,6 +8,15 @@ const authorize = require('../auth/authorize')
 const accountRouter = require('./account');
 const postRouter = require('./posts')
 
+const { body, validationResult } = require('express-validator');
+
+const createPostValidator = () => ([
+  body('title').trim().escape()
+ .isLength({max: 100, min: 10}),
+  body('content').trim().escape()
+  .isLength({max: 5000, min: 100})
+]);
+
 // get a list of posts
 router.get('/', async (req, res, next) => {
   const posts = await api.getPosts();
@@ -22,23 +31,34 @@ router.get('/:postId', async (req, res, next) => {
 });
 
 // verify jwt and create a new post
-router.post('/', authorize, async (req, res, next) => {
-  //TODO: validate form data
+router.post('/', authorize,
+  createPostValidator(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const aggregateError = new AggregateError(
+        errors.array().map((err) => new Error(err.msg)),
+        'Invalid input'
+      );
+      
+      return next(aggregateError);
+    }
 
-  const postData = {
-    title: req.body.title,
-    content: req.body.content,
+    const postData = {
+      title: req.body.title,
+      content: req.body.content,
+    }
+
+    let post;
+    try {
+      post = await createPost(postData, req.user.id);
+    } catch(err) {
+      return next(err);
+    }
+
+    res.json(post);
   }
-
-  let post;
-  try {
-    post = await createPost(postData, req.user.id);
-  } catch(err) {
-    return next(err);
-  }
-
-  res.json(post);
-});
+);
 
 
 module.exports = router;
