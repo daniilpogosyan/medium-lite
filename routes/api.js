57ee6d97dfa4354e951517ec10f6d193fc64c2b6
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-12-14 22:57:38 +0500

    Complete error handler in /api route

--- routes/api.js
+++ routes/api.js
@@ -9,6 +9,21 @@ router.use('/account', accountRouter);
 router.use('/posts', postRouter);
 
 router.use((err, req, res, next) => {
+  // AggregateError are created in request body validators
+  if (err.name === 'AggregateError') {
+    const errors = err.errors.map(error => {
+      return {
+        message: error.message
+      }
+    });
+
+    // if res got to error-handler but somehow still have code 200
+    if (res.statusCode === 200) {
+      res.status(500);
+    }
+    return res.json(errors)
+  } 
+
   res.json({message: err.message});
 });
 
