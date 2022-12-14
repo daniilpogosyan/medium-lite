3766febb88995022e7806392185a159e275a2018
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-12-14 20:01:15 +0500

    Send back jwt on signup and login

--- routes/account.js
+++ routes/account.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const api = require('../api');
+const issueJWT = require('../auth/issueJWT');
+const bcrypt = require('bcryptjs');
 
 // create a new user
-router.post('/signup', (req, res, next) => {
+router.post('/signup', async (req, res, next) => {
+  let user;
+  try {
+    user = await api.createUser(req.body.email, req.body.password);
+  } catch(err) {
+    return next(err);
+  }
 
+  const jwt = issueJWT({id: user.id});
+  res.json(jwt);
 });
 
 // verify credentials and get a jwt 
-router.post('/login', (req, res, next) => {
+router.post('/login', async (req, res, next) => {
+  let user;
+  user = await api.getUserByEmail(req.body.email);
 
+  if(user === null
+    || !bcrypt.compareSync(req.body.password, user.passwordHash)) {
+    const err = new Error('Wrong email or password');
+    return next(err);
+  }
+
+  const jwt = issueJWT({id: user.id});
+  res.json(jwt);
 });
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+const api = require('../api');
+
 const accountRouter = require('./account');
 const postRouter = require('./posts')
 
 // get a list of posts
-router.get('/', (req, res, next) => {
-
+router.get('/', async (req, res, next) => {
+  const posts = await api.getPosts();
+  res.json(posts);
 });
 
 
 // get a specific post
-router.get('/:postId', (req, res, next) => {
-  
+router.get('/:postId', async (req, res, next) => {
+  const post = await api.getPost(req.params.postId);
+  res.json(post);
 });
 
 // verify jwt and create a new post
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
   //TODO: validate form data
+  
   
 });
 
